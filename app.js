f7b1132e4d4fcfca45b74e5795fb6d8113ff8681
Dharmik Batra <batradharmik@gmail.com>
2020-11-19 00:04:17 +0530

    frontend almost final

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
   res.render("index");
 });
-
+app.get("/how",function(req,res){
+  res.render("how");
+});
+app.get("/calc",function(req,res){
+  res.render("calc");
+});
+app.post("/calc",function(req,res){
+  console.log(req.body);
+});
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
